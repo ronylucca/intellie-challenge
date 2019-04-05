cbb0a8a4eae88a191db95eb796f83c1de6b18568
Rony de Lucca <ronylucca@gmail.com>
2019-04-04 22:38:11 -0300

    Construct Implementations

---
+++ src/main/java/net/intelie/challenges/EventIteratorImpl.java
@@ -0,0 +1,34 @@
+package net.intelie.challenges;
+
+public class EventIteratorImpl implements EventIterator {
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean moveNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Event current() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		try {
+			
+		}catch(Exception e) {
+			
+		}
+
+	}
+
+}
---
+++ src/main/java/net/intelie/challenges/EventStoreImpl.java
@@ -0,0 +1,33 @@
+package net.intelie.challenges;
+
+public class EventStoreImpl implements EventStore {
+
+	@Override
+	public void insert(Event event) {
+		// TODO Auto-generated method stub
+		try {
+			
+		}catch(Exception e) {
+			
+		}
+
+	}
+
+	@Override
+	public void removeAll(String type) {
+		// TODO Auto-generated method stub
+		try {
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+	@Override
+	public EventIterator query(String type, long startTime, long endTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

c2253314d2fd28bfd3a3635935b262bccac90ce5
Rony de Lucca <ronylucca@gmail.com>
2019-04-05 01:00:21 -0300

    Clean Code operated

--- src/main/java/net/intelie/challenges/EventIteratorImpl.java
+++ src/main/java/net/intelie/challenges/EventIteratorImpl.java
@@ -1,34 +1,55 @@
 package net.intelie.challenges;
 
+import java.util.List;
+
 public class EventIteratorImpl implements EventIterator {
 
+    private final List<Event> events;
+    private final EventStoreImpl eventStore;
+    private int pointer = -1;
+    
+    EventIteratorImpl(EventStoreImpl eventStore, List<Event> events) {
+        this.eventStore = eventStore;
+        this.events = events;
+    }
+    
+    
 	@Override
 	public void close() throws Exception {
 		// TODO Auto-generated method stub
+		eventStore.closeEventIterator(this);
 
 	}
 
 	@Override
 	public boolean moveNext() {
 		// TODO Auto-generated method stub
-		return false;
+		pointer++;
+        if(pointer >= events.size()) {
+            return false;
+        }
+        return true;
 	}
 
 	@Override
 	public Event current() {
 		// TODO Auto-generated method stub
-		return null;
+		if(pointer == -1 || pointer >= events.size()) {
+            throw new IllegalStateException();
+        }
+        return events.get(pointer);
 	}
 
 	@Override
 	public void remove() {
 		// TODO Auto-generated method stub
-		try {
-			
-		}catch(Exception e) {
-			
-		}
 
+		if(pointer == -1 || pointer >= events.size()) {
+            throw new IllegalStateException();
+        }
+        eventStore.removeEvent(events.get(pointer));
+        events.remove(pointer);
+		
 	}
 
 }
--- src/main/java/net/intelie/challenges/EventStoreImpl.java
+++ src/main/java/net/intelie/challenges/EventStoreImpl.java
@@ -1,33 +1,149 @@
 package net.intelie.challenges;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class EventStoreImpl implements EventStore {
 
+	
+    private List<EventIterator> eventIterators = new LinkedList();
+    private HashMap<String, EventMenu> menu = new HashMap();
+    private Logger logger = Logger.getLogger(EventStoreImpl.class.getName());
+    
+    // concurrent object
+    Semaphore concurrentObject = new Semaphore(Integer.MAX_VALUE);
+    Semaphore concurrentEventIterators = new Semaphore(1);
+
+    private EventMenu getObjectListFiltered(final String type) {
+        return menu.get(type);
+
+    }
 	@Override
 	public void insert(Event event) {
 		// TODO Auto-generated method stub
+		
 		try {
+			//Thread safe begins
 			
-		}catch(Exception e) {
-			
-		}
-
+			   	concurrentObject.acquire(Integer.MAX_VALUE);
+	        	
+	        } catch (InterruptedException ex) {
+	            Logger.getLogger(EventStoreImpl.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	        EventMenu eventMenu = getObjectListFiltered(event.type());
+	        if (eventMenu == null) {
+	        	eventMenu = new EventMenu();
+	            menu.put(event.type(), eventMenu);
+	        }
+	        concurrentObject.release(Integer.MAX_VALUE);
+	        // Thread safe ends
+	        
+	        eventMenu.insertEvent(event);
+			Semaphore sm = new Semaphore(1);
+		
 	}
 
 	@Override
 	public void removeAll(String type) {
 		// TODO Auto-generated method stub
 		try {
+			//Thread safe begins
 			
-		}catch (Exception e) {
-			// TODO: handle exception
+			concurrentObject.acquire(Integer.MAX_VALUE);
+			menu.remove(type);
+			    // end of thread safe to edit the event lists
+			    
+			concurrentObject.release(Integer.MAX_VALUE);
+			
+			//Thread safe ends
+		}catch (InterruptedException e) {
+			logger.log(Level.SEVERE, null, e);
 		}
 
 	}
+	
 
 	@Override
 	public EventIterator query(String type, long startTime, long endTime) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		try {
+			//Thread safe begins
+			
+			concurrentObject.acquire(1);
+			
+			//Thread safe ends
+		}catch(InterruptedException e) {
+			logger.log(Level.SEVERE, null, e);
+		}
+		  EventMenu eventMenu = getObjectListFiltered(type);
+	        concurrentObject.release(1);
+	        // end of thread safe to get the Event Lists
+	        EventIteratorImpl eventIterator;
+	        if (eventMenu == null) {
+	            eventIterator = new EventIteratorImpl(this, new LinkedList());
+	            // thread safe to edit the list of iterators
+	            try {
+	            	concurrentEventIterators.acquire();
+	            } catch (InterruptedException ex) {
+	                Logger.getLogger(EventStoreImpl.class.getName()).log(Level.SEVERE, null, ex);
+	            }
+	            eventIterators.add(eventIterator);
+	            concurrentEventIterators.release();
+	            // end of thread safe to edit the list of iterators
+	            return eventIterator;
+	        }
+
+	        List<Event> list = eventMenu.query(startTime, endTime);
+	        eventIterator = new EventIteratorImpl(this, list);
+	        // Thread safe begins
+	        
+	        try {
+	            concurrentEventIterators.acquire();
+	        } catch (InterruptedException e) {
+	            logger.log(Level.SEVERE, null, e);
+	        }
+	        eventIterators.add(eventIterator);
+	        concurrentEventIterators.release();
+	        
+	        // Thread safe ends for iterators
+
+	        return eventIterator;
+			
 	}
+	
+	
+	 protected void closeEventIterator(EventIterator eventIerator) {
+		// TODO Auto-generated method stub   
+		// Thread safe begins
+	        try {
+	            concurrentEventIterators.acquire();
+	        } catch (InterruptedException ex) {
+	            logger.log(Level.SEVERE, null, ex);
+	        }
+	        eventIterators.remove(eventIerator);
+	        concurrentEventIterators.release();
+	        // Thread safe ends
+	}
+
+    protected void removeEvent(Event event) {
+        //Thread safe begins
+        try {
+            concurrentObject.acquire(Integer.MAX_VALUE);
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, null, e);
+        }
+        EventMenu eventList = getObjectListFiltered(event.type());
+        concurrentObject.release(Integer.MAX_VALUE);
+        
+        //Thread safe ends
+        if (eventList != null) {
+            eventList.removeEvent(event);
+        }
+    }
 
 }
